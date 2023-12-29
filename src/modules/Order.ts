6d36f2a44051d869f6dc8d9f9aebd39bc34e5a05
miaoluyao <128400398+miaoluyao@users.noreply.github.com>
2023-12-29 21:59:19 +0800

    Update Order.ts

--- src/modules/Order.ts
+++ src/modules/Order.ts
@@ -9,6 +9,8 @@ export function getAddressByNetwork(network: string): Address {
     return Address.fromString('0xd412054cca18a61278ced6f674a526a6940ebd84')
   } if (network == 'rinkeby') {
     return Address.fromString('0xd412054cca18a61278ced6f674a526a6940ebd84')
+  } if (network == 'bsc') {
+    return Address.fromString('0x74C58472f67A698DD12D61f82c3Cb0474fE9851e')
   }
 
   throw 'No Address specified'
